13487cf30097d7a4604b7d42f3a4cd7835417fbf
windyXu <1390683299@qq.com>
2022-09-07 23:38:12 +0800

    输入框事件

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
 import MyHeader from "./components/Header";
 import AddInput from "./components/AddInput";
 
 function App() {
-  const [isInputShow, setIsInputShow] = useState(false);
+  const [isInputShow, setIsInputShow] = useState(false),
+    [todoList, setTodoList] = useState([]);
+  const addItem = useCallback((value) => {
+    const dataItem = {
+      id: new Date().getTime(),
+      content: value,
+      completed: false,
+    };
 
+    setTodoList((todoList) => [...todoList, dataItem]);
+    setIsInputShow(false);
+  }, []);
   return (
     <div className="App">
       <MyHeader openInput={() => setIsInputShow(!isInputShow)} />
-      <AddInput isInputShow={isInputShow} />
+      <AddInput isInputShow={isInputShow} addItem={addItem} />
     </div>
   );
 }
--- src/components/AddInput/index.js
+++ src/components/AddInput/index.js
@@ -1,15 +1,28 @@
-import react from "react";
+import react, { useRef } from "react";
 
 import "./index.scss";
 
 function AddInput(props) {
-  const { isInputShow } = props;
+  const { isInputShow, addItem } = props,
+    inputRef = useRef(null);
+  const submitValue = () => {
+    const inputValue = inputRef.current.value.trim();
+
+    if (inputValue.length === 0) {
+      return;
+    }
+    addItem(inputValue);
+    inputRef.current.value = "";
+  };
+
   return (
     <>
       {isInputShow ? (
         <div className="input-wrapper">
-          <input type="text" placeholder="请输入待办事件" />
-          <button className="btn btn-primary">增加</button>
+          <input type="text" placeholder="请输入待办事件" ref={inputRef} />
+          <button className="btn btn-primary" onClick={submitValue}>
+            增加
+          </button>
         </div>
       ) : (
         ""
